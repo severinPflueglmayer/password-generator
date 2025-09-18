781f36535aea345dbba4e17e8668abd0df6a71e1
severinPflueglmayer <severin.pflueglmayer@gmail.com>
2025-09-18 09:13:26 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function generatePassword(length = 10) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+
+  return password;
+}
+
+
+MediaSourceHandle.exports = generatePassword();
